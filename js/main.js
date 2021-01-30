06aa9df234a6fca3f7ab939ca22f435d2594fc7e
kvzoloto <kirill2000zol@gmail.com>
2021-01-30 15:35:08 +0200

    commit2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$("#navToggle").click(function() {
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("open");
+    // this line ▼ prevents content scroll-behind
+    $("body").toggleClass("locked");
+});
+$('.overlay').click(function() {
+    $(this).removeClass('open');
+    $('.navBurger').removeClass('active');
+});
 
